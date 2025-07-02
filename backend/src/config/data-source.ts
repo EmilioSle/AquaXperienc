92a793d350624d38a982224436ecaa79c39bf24a
Walthergl09 <waltherjr68@gmail.com>
2025-07-02 16:38:37 -0500

    para coneccion a bd

--- backend/src/config/data-source.ts
+++ backend/src/config/data-source.ts
@@ -4,18 +4,15 @@ import { DataSource } from 'typeorm';
 import dotenv from 'dotenv';
 import { Usuario } from '../models/usuario.model';
 
-// Cargar las variables de entorno desde el archivo .env
 dotenv.config();
 
-// Configuración de la conexión a la base de datos
 export const AppDataSource = new DataSource({
-  type: process.env.DB_TYPE as 'postgres',
-  host: process.env.DB_HOST,
-  port: parseInt(process.env.DB_PORT || '5432'),
-  username: process.env.DB_USERNAME,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE,
-  entities: [Usuario],
-  synchronize: process.env.DB_SYNCHRONIZE === 'true',
-  logging: true,
+  type: 'postgres', 
+  url: process.env.DB_URL, 
+  ssl: {
+    rejectUnauthorized: false, 
+  },
+  entities: [Usuario], 
+  synchronize: process.env.DB_SYNCHRONIZE === 'true', 
+  logging: true, 
 });
