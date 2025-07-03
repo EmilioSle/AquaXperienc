8db0891c774dae170428983a8ff68b91ecc5e977
EmilioSle <emiliosleimen555@gmail.com>
2025-07-03 17:48:55 -0500

    si

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,16 +1,9 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import LoginPage from './pages/login/page';
-import RegisterPage from './pages/register/page';
-import BeginPage from './pages/begin/page';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
 
-const App = () => (
-  <BrowserRouter>
-    <Routes>
-      <Route path="/login" element={<LoginPage />} />
-      <Route path="/register" element={<RegisterPage />} />
-      <Route path="/begin" element={<BeginPage />} />
-    </Routes>
-  </BrowserRouter>
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
-
-export default App;
